99951b0959762ffa724468e68db3aa2112206bd4
Mr Chen <midoks@163.com>
2025-01-05 01:04:44 +0800

    Update init.go

--- internal/routers/init.go
+++ internal/routers/init.go
@@ -11,8 +11,7 @@ import (
 func InitRouters() {
 	engine := gin.Default()
 	engine.SetTrustedProxies(nil)
-	gin.SetMode(gin.ReleaseMode)
-
+	// gin.SetMode(gin.ReleaseMode)
 	port := fmt.Sprintf(":%d", conf.Http.Port)
 	engine.Run(port)
 }
