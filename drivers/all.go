d01b6832c8dcbac5372a2284d40170c525d1e37d
Mr Chen <midoks@163.com>
2025-02-01 23:54:51 +0800

    up

---
+++ drivers/all.go
@@ -0,0 +1,11 @@
+package drivers
+
+import (
+	_ "ndm/drivers/local"
+)
+
+// All do nothing,just for import
+// same as _ import
+func All() {
+
+}
---
+++ internal/routers/storage.go
@@ -0,0 +1,30 @@
+package bootstrap
+
+import (
+	"context"
+
+	"ndm/internal/conf"
+	"ndm/internal/db"
+	"ndm/internal/model"
+	"ndm/internal/op"
+	"ndm/pkg/utils"
+)
+
+func LoadStorages() {
+	storages, err := db.GetEnabledStorages()
+	if err != nil {
+		utils.Log.Fatalf("failed get enabled storages: %+v", err)
+	}
+	go func(storages []model.Storage) {
+		for i := range storages {
+			err := op.LoadStorage(context.Background(), storages[i])
+			if err != nil {
+				utils.Log.Errorf("failed get enabled storages: %+v", err)
+			} else {
+				utils.Log.Infof("success load storage: [%s], driver: [%s], order: [%d]",
+					storages[i].MountPath, storages[i].Driver, storages[i].Order)
+			}
+		}
+		conf.StoragesLoaded = true
+	}(storages)
+}
