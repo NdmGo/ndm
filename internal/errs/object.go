1949d5272449a2f290bb5e42d72a8f532c712c78
dami <midoks@163.com>
2025-06-28 14:56:40 +0800

    Update object.go

--- internal/errs/object.go
+++ internal/errs/object.go
@@ -10,6 +10,13 @@ var (
 	ObjectNotFound = errors.New("object not found")
 	NotFolder      = errors.New("not a folder")
 	NotFile        = errors.New("not a file")
+
+	FailCreateDir = errors.New("failed to create directory")
+
+	//backup
+	NotEnbleBackup      = errors.New("backup not supported")
+	DirNotSupportBackup = errors.New("directory does not support backup")
+	BackupDirNotExist   = errors.New("backup dir does not exist")
 )
 
 func IsObjectNotFound(err error) bool {
