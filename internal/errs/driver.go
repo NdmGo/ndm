09e0c8426ca28b60cb84087eac614d530908ddc0
dami <midoks@163.com>
2025-06-29 22:28:19 +0800

    Update driver.go

--- internal/errs/driver.go
+++ internal/errs/driver.go
@@ -3,5 +3,6 @@ package errs
 import "errors"
 
 var (
-	EmptyToken = errors.New("empty token")
+	EmptyToken           = errors.New("empty token")
+	MountPathCannotEmpty = errors.New("mounting path cannot be empty")
 )
