bd32eb34a27df35b0b600683a016b8e3f904b6d9
dami <midoks@163.com>
2025-06-29 19:33:18 +0800

    Update multitasking.go

--- internal/utils/multitasking/multitasking.go
+++ internal/utils/multitasking/multitasking.go
@@ -2,7 +2,10 @@ package multitasking
 
 import (
 	// "strings"
+	"fmt"
+	"log"
 	"sync"
+	// "time"
 )
 
 const (
@@ -21,10 +24,15 @@ type DownloadTask struct {
 }
 
 type MultiTasking struct {
-	limit     int64
+	limit            int64
+	current_task_num int64
+
 	taskChan  chan DownloadTask
 	results   chan string
 	errorChan chan error
+	done      chan bool
+
+	task chan func()
 }
 
 func Instance() *MultiTasking {
@@ -35,13 +43,40 @@ func Instance() *MultiTasking {
 	return instance
 }
 
+func Factory() *MultiTasking {
+	f := &MultiTasking{}
+	f.Init(10)
+	return t
+}
+
 func (mt *MultiTasking) Init(limit int64) error {
 	mt.limit = limit
+	mt.current_task_num = 0
 
 	// create task channels and work pools
 	mt.taskChan = make(chan DownloadTask, mt.limit*2)
 	mt.results = make(chan string, mt.limit*2)
 	mt.errorChan = make(chan error, mt.limit*2)
+	mt.task = make(chan func(), mt.limit)
+
+	mt.done = make(chan bool)
+	go func() {
+		var successCount, failCount int
+		for {
+			select {
+			case <-mt.results:
+				successCount++
+			case err := <-mt.errorChan:
+				log.Printf("下载失败: %v", err)
+				failCount++
+			case <-mt.done:
+				fmt.Printf("\n下载完成! 成功: %d, 失败: %d\n", successCount, failCount)
+				return
+			}
+		}
+	}()
+
+	go mt.do()
 	return nil
 }
 
@@ -49,9 +84,43 @@ func (mt *MultiTasking) GetTaskLimit() int64 {
 	return mt.limit
 }
 
+func (mt *MultiTasking) do() {
+	for {
+		select {
+		case fn := <-mt.task:
+			defer wg.Done()
+			mt.current_task_num -= 1
+			fmt.Println("task:", len(mt.task))
+			fmt.Println("do....")
+			fn()
+
+			// case err := <-mt.errorChan:
+			// 	log.Printf("下载失败: %v", err)
+			// 	failCount++
+			// case <-mt.done:
+			// 	fmt.Printf("\n下载完成! 成功: %d, 失败: %d\n", successCount, failCount)
+			// 	return
+		}
+	}
+
+	// for _, fn := range mt.task {
+	// 	fn()
+	// 	defer wg.Done()
+	// 	mt.current_task_num -= 1
+
+	// }
+}
+
 func (mt *MultiTasking) DoneTask(fn func()) {
 	wg.Add(1)
-	fn()
+	mt.current_task_num += 1
+	mt.task <- fn
+}
 
-	wg.Done()
+func (mt *MultiTasking) Close() {
+	close(mt.taskChan)
+	wg.Wait()
+	close(mt.results)
+	close(mt.errorChan)
+	mt.done <- true
 }
